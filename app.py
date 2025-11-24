2aa17e7a7bae30abe03e6294ae08b8522aa23f2f
tsesunny1003 <tsesunny1003@gmail.com>
2025-11-24 13:55:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
-
-import pandas as pd
 import streamlit as st
-from numpy.random import default_rng as rng
+from streamlit_option_menu import option_menu
 
-df = pd.DataFrame(rng(0).standard_normal((20,3)), columns=["a", "b", "c"])
+st.title('Hello, Students!')
+st.write('This is your Python Programming course.')
 
-st.area_chart(df)
+with st.sidebar:
+    selected=option_menu(
+        menu_title = "Menu",
+        options = ["Home", "About", "Contact"],
+        icons = ["1-circle-fill",
+                 "2-circle-fill",
+                 "3-circle-fill"],
+        menu_icon= "emoji-smile-fill",
+        default_index=0,
+    )
 
+if selected == "Home":
+    st.title(f"Welcome to the {selected} page.")
 
+if selected == "About":
+    st.title(f"Welcome to the {selected} page.")
 
-
-
-option = st.selection(
-    "How would you like to be contacted?", 
-    ["Email?",
-    "Home phone",
-    "Mobile phone"],
-)
-
-
-st.write("You selected:", option)
+if selected == "Contact":
+    st.title(f"Welcome to the {selected} page.")
